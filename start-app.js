284d312c456075329e67ed5a4851e5b298c746f1
rafaelyu <mingzhi.yu@yale.edu>
2015-12-03 21:00:59 +0000

    new change

--- start-app.js
+++ start-app.js
@@ -10,14 +10,10 @@ var host = process.env.C9_HOSTNAME || process.env.IP;
 app.listen(port, function() {
   
   if (process.env.C9_HOSTNAME) {
-
     // Looks like we're on Cloud9, print a nice message for students.
     console.log('Your app is running at http://' + host);  
-  
-  }else{
-  
+  } else{
     // We're running somewhere else, just print the port number.
     console.log('Your app is running on PORT ', port);
-  
   }
 });
